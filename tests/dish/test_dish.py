4118b7d8bbbbbfb44e3d40533810e90a6d03a5b9
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-16 18:29:56 -0300

    Refatorando req 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,32 +1,26 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
-from src.models.ingredient import Ingredient
+from src.models.ingredient import Ingredient, Restriction
 import pytest
 
 
 # Req 2
 def test_dish():
-    pancake = 'panqueca'
-    pizza = 'pizza'
-    instance_pancake = Dish(pancake, 20.00)
-    instance_pizza = Dish(pizza, 45.00)
+    item = Ingredient("ovo")
+    instance_pancake = Dish('panqueca', 20.00)
+    instance_pizza = Dish('pizza', 45.00)
 
-    dish_invalid = 'feijoada'
-    dish_not_flexible = 'Dish price must be float'
-    value_bigger_of_zero = 'Dish price must be greater then zero'
+    with pytest.raises(TypeError):
+        Dish('panqueca', 'lasanha')
+    with pytest.raises(ValueError):
+        Dish('panqueca', 0)
 
-    with pytest.raises(TypeError, match=dish_not_flexible):
-        Dish(dish_invalid, dish_invalid)
-    with pytest.raises(ValueError, match=value_bigger_of_zero):
-        Dish(dish_invalid, -17)
+    assert instance_pancake.__eq__(instance_pancake) is True
+    assert instance_pancake.__eq__(instance_pizza) is False
+    assert hash(instance_pancake) == hash(instance_pancake)
+    assert hash(instance_pancake) != hash(instance_pizza)
+    assert repr(instance_pancake) == "Dish('panqueca', R$20.00)"
 
-    assert instance_pancake == instance_pancake
-    assert instance_pancake != instance_pizza
-    assert hash(instance_pancake) == hash(repr(instance_pancake))
-    assert repr(instance_pancake) == f"Dish('{pancake}', R$20.00)"
-    assert instance_pancake.name == pancake
-
-    instance_pancake.add_ingredient_dependency(Ingredient('testing'), 50)
-    assert instance_pancake.recipe == {Ingredient('testing'): 50}
-    assert instance_pancake.get_restrictions() == set()
-    assert instance_pancake.get_ingredients() == set(
-        instance_pancake.recipe.keys())
+    instance_pancake.add_ingredient_dependency(item, 1)
+    assert instance_pancake.name == 'panqueca'
+    assert instance_pancake.get_restrictions() == {Restriction.ANIMAL_DERIVED}
+    assert instance_pancake.get_ingredients() == {Ingredient("ovo")}
