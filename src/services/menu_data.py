a78f5e9010ed56029a2a15fe9b9a5cc73d055b5a
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-16 19:01:48 -0300

    Requisito 3

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,19 @@
-# Req 3
+from src.models.dish import Dish
+from src.models.ingredient import Ingredient
+import pandas as pd
+
+
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, source_path):
+        self.recipes = set()
+        self.df = pd.read_csv(source_path)
+
+        recipes = {}
+        for items in self.df.itertuples(index=False):
+            dish, price, ingr, amount = items
+            if dish not in recipes:
+                menu = Dish(dish, price)
+                recipes[dish] = menu
+                self.recipes.add(menu)
+            item_info = Ingredient(ingr)
+            recipes[dish].add_ingredient_dependency(item_info, amount)
