9c9dad0d62b467460f6ddc3b7591cd9420c95ea7
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2023-05-16 10:43:36 -0300

    Requisito 1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient
 
 
-# Req 1
 def test_ingredient():
-    pass
+    potato = 'batata'
+    tomato = 'tomate'
+    instance_batata = Ingredient(potato)
+    instance_tomato = Ingredient(tomato)
+
+    assert instance_batata == instance_batata
+    assert instance_batata != instance_tomato
+    assert repr(instance_batata) == f"Ingredient('{potato}')"
+    assert hash(instance_batata) == hash(potato)
+    assert instance_batata.restrictions == set()
+    assert instance_batata.name == potato
